9f1a650e51fa237ba687c488eb881162bd0cf2e1
Candy0707 <j890707@gmail.com>
2025-09-04 22:56:53 +0800

    測試顯示按鈕變化

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -432,14 +432,17 @@ class SelfdriveD(CruiseHelper):
 
     CruiseHelper.update(self, CS, self.events_sp, self.experimental_mode)
 
-    # decrement personality on distance button press
-    if self.CP.openpilotLongitudinalControl:
-      if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents):
-        if not self.experimental_mode_switched:
-          self.personality = (self.personality - 1) % 3
-          self.params.put_nonblocking('LongitudinalPersonality', self.personality)
-          self.events.add(EventName.personalityChanged)
-        self.experimental_mode_switched = False
+    for be in CS.buttonEvents:
+     # decrement personality on distance button press
+      if self.CP.openpilotLongitudinalControl:
+        if any(not be.pressed and be.type == ButtonType.gapAdjustCruise):
+          if not self.experimental_mode_switched:
+            self.personality = (self.personality - 1) % 3
+            self.params.put_nonblocking('LongitudinalPersonality', self.personality)
+            self.events.add(EventName.personalityChanged)
+          self.experimental_mode_switched = False
+      if be.type == ButtonType.lkas:
+        self.events.add(EventName.lksa)
 
   def data_sample(self):
     _car_state = messaging.recv_one(self.car_state_sock)
