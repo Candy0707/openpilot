abdf3adac7760f70d64061028f5470455e7d2c10
Candy0707 <j890707@gmail.com>
2026-01-24 16:27:05 +0800

    修改視覺顯示 開發者UI 與 雷達UI

--- selfdrive/ui/sunnypilot/layouts/settings/visuals.py
+++ selfdrive/ui/sunnypilot/layouts/settings/visuals.py
@@ -4,15 +4,15 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+
 from openpilot.common.params import Params
 from openpilot.system.ui.lib.multilang import tr, tr_noop
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.list_view import multiple_button_item
 
-DESCRIPTIONS = {
-  "DeveloperUI": tr_noop("Developer UI")
-}
+DESCRIPTIONS = {"DeveloperUI": tr_noop("Developer UI"), "RadarInfoUI": tr_noop("Radar Info UI")}
+
 
 class VisualsLayout(Widget):
   def __init__(self):
@@ -23,18 +23,26 @@ class VisualsLayout(Widget):
     self._scroller = Scroller(items, line_separator=True, spacing=0)
 
   def _initialize_items(self):
-    self._long_personality_setting = multiple_button_item(
+    self._DeveloperUI_Setting = multiple_button_item(
       lambda: tr("Developer UI"),
       lambda: tr(DESCRIPTIONS["DeveloperUI"]),
       buttons=[lambda: tr("OFF"), lambda: tr("RIGHT"), lambda: tr("BOTTOM"), lambda: tr("BOTH")],
       button_width=240,
       callback=self._set_developer_ui,
       selected_index=self._params.get("DevUIInfo", return_default=True),
-      icon="speed_limit.png"
+      icon="speed_limit.png",
     )
-    items = [
-      self._long_personality_setting
-    ]
+    self._RadarInfoUI_Setting = multiple_button_item(
+      lambda: tr("Radar Info UI"),
+      lambda: tr(DESCRIPTIONS["RadarInfoUI"]),
+      buttons=[lambda: tr("OFF"), lambda: tr("Point"), lambda: tr("Info")],
+      button_width=240,
+      callback=self._set_radarinfo_ui,
+      selected_index=self._params.get("RadarUIInfo", return_default=True),
+    )
+    items =[self._DeveloperUI_Setting,
+            self._RadarInfoUI_Setting
+          ]
     return items
 
   def _render(self, rect):
@@ -45,3 +53,6 @@ class VisualsLayout(Widget):
 
   def _set_developer_ui(self, button_index: int):
     self._params.put("DevUIInfo", button_index)
+
+  def _set_radarinfo_ui(self, button_index: int):
+    self._params.put("RadarUIInfo", button_index)
