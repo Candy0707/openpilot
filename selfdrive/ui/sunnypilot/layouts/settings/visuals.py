bab2dbb0d396f06a79504059c665c840f3f114e1
Candy0707 <j890707@gmail.com>
2026-01-29 18:25:31 +0800

    新增選項

--- selfdrive/ui/sunnypilot/layouts/settings/visuals.py
+++ selfdrive/ui/sunnypilot/layouts/settings/visuals.py
@@ -11,7 +11,11 @@ from openpilot.system.ui.widgets.scroller_tici import Scroller
 from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.list_view import multiple_button_item
 
-DESCRIPTIONS = {"DeveloperUI": tr_noop("Developer UI"), "RadarInfoUI": tr_noop("Radar Info UI")}
+DESCRIPTIONS = {
+  "DeveloperUI": tr_noop("Developer UI"),
+  "RadarInfoUI": tr_noop("Radar Info UI"),
+  "ChevronUI": tr_noop("Chevron UI"),
+}
 
 
 class VisualsLayout(Widget):
@@ -40,9 +44,15 @@ class VisualsLayout(Widget):
       callback=self._set_radarinfo_ui,
       selected_index=self._params.get("RadarUIInfo", return_default=True),
     )
-    items =[self._DeveloperUI_Setting,
-            self._RadarInfoUI_Setting
-          ]
+    self._ChevronUI_Setting = multiple_button_item(
+      lambda: tr("Chevron UI"),
+      lambda: tr(DESCRIPTIONS["ChevronUI"]),
+      buttons=[lambda: tr("OFF"), lambda: tr("DISTANCE_ONLY"), lambda: tr("SPEED_ONLY"), lambda: tr("TTC_ONLY"), lambda: tr("ALL")],
+      button_width=240,
+      callback=self._set_chevron_ui,
+      selected_index=self._params.get("ChevronInfo", return_default=True),
+    )
+    items = [self._DeveloperUI_Setting, self._RadarInfoUI_Setting, self._ChevronUI_Setting]
     return items
 
   def _render(self, rect):
@@ -56,3 +66,6 @@ class VisualsLayout(Widget):
 
   def _set_radarinfo_ui(self, button_index: int):
     self._params.put("RadarUIInfo", button_index)
+
+  def _set_chevron_ui(self, button_index: int):
+    self._params.put("ChevronInfo", button_index)
