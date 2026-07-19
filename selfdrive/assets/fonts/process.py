08bbe6762134e42adcc1e5f46b1b2d2453d861bb
Candy <j890707@gmail.com>
2026-07-19 14:47:22 +0800

    [優化] 降低 字體大小 100 -> 64

--- selfdrive/assets/fonts/process.py
+++ selfdrive/assets/fonts/process.py
@@ -79,7 +79,7 @@ def _process_font(font_path: Path, codepoints: tuple[int, ...]):
 
   font_size = {
     "unifont.otf": 16,  # unifont is only 16x8 or 16x16 pixels per glyph
-  }.get(font_path.name, 100)
+  }.get(font_path.name, 64)
 
   data = font_path.read_bytes()
   file_buf = rl.ffi.new("unsigned char[]", data)
@@ -90,7 +90,7 @@ def _process_font(font_path: Path, codepoints: tuple[int, ...]):
     rl.ffi.cast("unsigned char *", file_buf), len(data), font_size, cp_ptr, len(codepoints),
     rl.FontType.FONT_DEFAULT, glyph_count
   )
-    if glyphs == rl.ffi.NULL:
+  if glyphs == rl.ffi.NULL:
     raise RuntimeError("raylib failed to load font data")
 
   rects_ptr = rl.ffi.new("Rectangle **")
