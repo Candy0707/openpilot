a06d4ac6cc09825d16b5b227d5a7d5f4e86fde78
Candy0707 <93701039+Candy0707@users.noreply.github.com>
2026-07-21 16:13:09 +0800

    Update import statements for cereal module

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from openpilot.cereal import custom
+from cereal import custom
 from opendbc.car.structs import car
 from openpilot.common.gps import get_gps_location_service
 from openpilot.common.params import Params
@@ -7,7 +7,7 @@ from openpilot.common.realtime import Priority, config_realtime_process
 from openpilot.common.swaglog import cloudlog
 from openpilot.selfdrive.controls.lib.ldw import LaneDepartureWarning
 from openpilot.selfdrive.controls.lib.longitudinal_planner import LongitudinalPlanner
-import openpilot.cereal.messaging as messaging
+import cereal.messaging as messaging
 
 
 def main():
