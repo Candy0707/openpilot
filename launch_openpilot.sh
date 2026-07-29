d2c20260541417d828e252d5d0343007ab239d21
herizon1054 <spivbaby@gmail.com>
2026-07-30 02:21:10 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -16,5 +16,7 @@ if [ "$MODEL" = "comma tici" ]; then
   # If it exists, run it
   exec "$C3_LAUNCH_SH"
 fi
-
+echo -n "2" > /data/params/d/HasAcceptedTerms
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
+echo -n "1" > /data/params/d/IsMetric
 exec ./launch_chffrplus.sh
