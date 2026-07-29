907e55f80828f6ad380201067d81e6f2970249b0
herizon1054 <spivbaby@gmail.com>
2026-07-30 01:00:47 +0800

    Update services.py

--- cereal/services.py
+++ cereal/services.py
@@ -116,6 +116,7 @@ _services: dict[str, tuple] = {
   "customReservedRawData0": (True, 0.),
   "customReservedRawData1": (True, 0.),
   "customReservedRawData2": (True, 0.),
+  "tdx": (True, 0., 1),
 }
 SERVICE_LIST = {name: Service(*vals) for
                 idx, (name, vals) in enumerate(_services.items())}
