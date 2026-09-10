479684389b6e27bbf569a2b21f25facf4b228bb5
Candy <j890707@gmail.com>
2026-09-10 23:34:32 +0800

    [修復] 動態角度引用

--- openpilot/selfdrive/controls/controlsd.py
+++ openpilot/selfdrive/controls/controlsd.py
@@ -150,11 +150,21 @@ class Controls(ControlsExt):
     steer, lateral_output, lac_log = self.LaC.update(CC.latActive, CS, self.VM, lp,
                                                        self.steer_limited_by_safety, self.desired_curvature,
                                                        self.calibrated_pose, curvature_limited, lat_delay)
-    actuators.torque = float(steer)
-    if self.CP.steerControlType == car.CarParams.SteerControlType.curvature:
-      actuators.curvature = float(lateral_output)
+    # --- 讀取大腦內部決定並寫入 Enum ---
+    if hasattr(self.LaC, 'use_angle'):
+      if self.LaC.use_angle:
+        actuators.steerControlType = car.CarControl.Actuators.SteerControlType.angle
+      else:
+        actuators.steerControlType = car.CarControl.Actuators.SteerControlType.torque
     else:
-      actuators.steeringAngleDeg = float(lateral_output)
+      if self.CP.steerControlType == car.CarParams.SteerControlType.angle:
+        actuators.steerControlType = car.CarControl.Actuators.SteerControlType.angle
+      else:
+        actuators.steerControlType = car.CarControl.Actuators.SteerControlType.torque
+    # -----------------------------------
+    actuators.torque = float(steer)
+    actuators.curvature = float(lateral_output)
+    actuators.steeringAngleDeg = float(lateral_output)
     # Ensure no NaNs/Infs
     for p in ACTUATOR_FIELDS:
       attr = getattr(actuators, p)
